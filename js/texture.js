b0d5cea82a54d9d0f05fc4b34060db51f3aaeb85
shygoo <shyguyhex@gmail.com>
2020-07-15 02:03:08 -0500

    add item textures

--- js/texture.js
+++ js/texture.js
@@ -42,3 +42,58 @@ function rgba32_texture_from_ci4(dv, ciOffset, tlutOffset, width, height)
 
     return data;
 }
+
+function rgba32_texture_from_i8(dv, offset, width, height)
+{
+    var numTexels = width * height;
+    var data = new Uint8Array(numTexels * 4);
+
+    for(var i = 0; i < numTexels; i++)
+    {
+        var byte = dv.getUint8(offset + i);
+        data[i*4+0] = byte;
+        data[i*4+1] = byte;
+        data[i*4+2] = byte;
+        data[i*4+3] = byte;
+    }
+
+    return data;
+}
+
+function rgba32_texture_from_i4(dv, offset, width, height)
+{
+    var numTexels = width * height;
+    var data = new Uint8Array(numTexels * 4);
+
+    for(var i = 0; i < numTexels/2; i++)
+    {
+        var byte = dv.getUint8(offset + i);
+        var t0 = 0x11 * ((byte & 0xF0) >> 4);
+        var t1 = 0x11 * (byte & 0x0F);
+        data[i*8+0] = t0;
+        data[i*8+1] = t0;
+        data[i*8+2] = t0;
+        data[i*8+3] = t0;
+        data[i*8+4] = t1;
+        data[i*8+5] = t1;
+        data[i*8+6] = t1;
+        data[i*8+7] = t1;
+    }
+
+    return data;
+}
+
+// print texture to the console
+function print_rgba32_texture(texture, width, height)
+{
+    var array = new Uint8ClampedArray(texture.buffer);
+    var imageData = new ImageData(array, width, height);
+
+    var ctx = document.createElement('canvas').getContext('2d');
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    ctx.putImageData(imageData, 0, 0, 0, 0, width, height);
+
+    var url = ctx.canvas.toDataURL()
+    console.log('%c ', 'border: 1px solid #000; font-size: 0px; background-image: url("'+url+'"); padding: ' + (width/2) + 'px ' + (height/2) + 'px;');
+}
