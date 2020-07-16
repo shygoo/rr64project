aab5346dc3e965b336e3f85dc7e6cf1d8dfe3995
shygoo <shyguyhex@gmail.com>
2020-07-16 17:35:46 -0500

    refactor

--- js/rr64.js
+++ js/rr64.js
@@ -9,11 +9,11 @@ const RA_UNK_FILE = 0x000AA4C0;
 const RA_PATH_DATA = RA_UNK_FILE + 0x418;
 const NUM_PATH_POINTS = 3053;
 
-const RA_FILE_TABLE = 0x0018D380; 
-const NUM_FILES = 0x16C2;
+const RA_MAP_MODEL_TABLE = 0x0018D380; 
+const NUM_MAP_MODELS = 0x16C2;
 
 // 0101D440: 00000040 00248800 00000001 00000175 // (175 = number of models)
-const RA_MODEL_TABLE_HEADER = 0x0101D440; 
+const RA_MODEL_TABLE_HEADER = 0x0101D440;
 const RA_MODEL_TABLE = 0x0101D480;
 const NUM_MODELS = 0x175;
 
@@ -21,6 +21,7 @@ const RA_OBJECT_PLACEMENT_TABLE = 0x01265C50;
 const NUM_PLACEMENT_FILES = 4900; // is this correct?
 
 const RA_OBJECT_DEFINITION_TABLE = 0x01303208;
+const NUM_OBJECT_DEFINITIONS = 0x127; // todo use header
 
 const RA_ITEM_COUNT = 0x01304DC0;
 const RA_ITEM_TABLE = 0x01304DC4;
@@ -28,14 +29,360 @@ const RA_ITEM_TABLE = 0x01304DC4;
 const RA_RACE_TABLE = 0x000B67C4;
 const NUM_RACES = 58;
 
-const SIG_TEXTURE      = 0x16;
-const SIG_ANIM_TEXTURE = 0x17;
-const SIG_SUBMESH      = 0x3D;
-const SIG_FILE_TABLE   = 0x3E;
-const SIG_MESH         = 0x3F;
+// TODO these are part of a larger collection of textures referenced by the table at 0x01E09368, should eventually use that instead
+const RA_ITEM_TEXTURES = 0x01F49730;
+const NUM_ITEM_TEXTURES = 16;
+const ITEM_TEX_INDICES = [ // todo where is this defined?
+    null,
+    0x0D, 0x0E, 0x0F, 0x0C, 0x06, 0x00, 0x02, 0x05,
+    0x09, 0x08, 0x01, 0x03, 0x0B, 0x0A, 0x07, 0x04
+];
+
+const SIG_TEXTURE      = 0x00000016;
+const SIG_ANIM_TEXTURE = 0x00000017;
+const SIG_SUBMESH      = 0x0000003D;
+const SIG_FILE_TABLE   = 0x0000003E;
+const SIG_MESH         = 0x0000003F;
 
 const TEX_FMT_CI4      = 0x04;
 const TEX_FMT_CI8      = 0x08;
+
+///////////////////
+
+function RoadRash64(dvRom)
+{
+    this.dvRom = dvRom;
+
+    this.mapTextures = this.parseMapTextures();
+    this.mapModels = this.parseMapModels();
+    this.objectDefinitions = this.parseObjectDefinitions();
+    this.objectModels = this.parseObjectModels();
+    this.objectPlacements = this.parseObjectPlacements();
+    this.itemTextures = this.parseItemTextures();
+    this.itemPlacements = this.parseItemPlacements();
+}
+
+RoadRash64.prototype.parseMapTextures = function()
+{
+    var mapTextures = [];
+
+    // note: map textures and models share the same table
+    for(var i = 0; i < NUM_MAP_MODELS; i++)
+    {
+        // 0x18 = file system header size
+        var entryOffset = RA_MAP_MODEL_TABLE + 0x18 + (i * FileEntry.SIZE);
+        var fileEntry = new FileEntry(this.dvRom, entryOffset)
+
+        if(fileEntry.offset == 0)
+        {
+            mapTextures.push(null);
+            continue;
+        }
+
+        var fileAddress = fileEntry.offset + entryOffset;
+        var signature = this.dvRom.getUint32(fileAddress + 0x00);
+        var dvFile = dvpart(this.dvRom, fileAddress, fileEntry.size)
+
+        if(signature == SIG_TEXTURE || signature == SIG_ANIM_TEXTURE)
+        {
+            var textureFile = this.parseTextureFile(dvFile);
+            mapTextures.push(textureFile);
+        }
+        else
+        {
+            mapTextures.push(null);
+        }
+    }
+
+    return mapTextures;
+}
+
+RoadRash64.prototype.parseMapModels = function()
+{
+    var mapModels = [];
+
+    // note: map textures and models share the same table
+    for(var i = 0; i < NUM_MAP_MODELS; i++)
+    {
+        // 0x18 = file system header size
+        var entryOffset = RA_MAP_MODEL_TABLE + 0x18 + (i * FileEntry.SIZE);
+        var fileEntry = new FileEntry(this.dvRom, entryOffset)
+
+        if(fileEntry.offset == 0)
+        {
+            continue;
+        }
+
+        var fileAddress = fileEntry.offset + entryOffset;
+        var signature = this.dvRom.getUint32(fileAddress + 0x00);
+        var dvFile = dvpart(this.dvRom, fileAddress, fileEntry.size)
+
+        if(signature == SIG_MESH) // mesh file
+        {
+            var mapModel = this.parseMapMesh(dvFile);
+            mapModels.push(mapModel);
+        }
+    }
+
+    return mapModels;
+}
+
+RoadRash64.prototype.parseObjectDefinitions = function()
+{
+    var objectDefinitions = [];
+
+    for(var nObjectDef = 0; nObjectDef < NUM_OBJECT_DEFINITIONS; nObjectDef++)
+    {
+        var objectDef = new ObjectDefinition(this.dvRom, RA_OBJECT_DEFINITION_TABLE + nObjectDef * ObjectDefinition.SIZE);
+        objectDefinitions.push(objectDef);
+    }
+
+    return objectDefinitions;
+}
+
+RoadRash64.prototype.parseObjectModels = function()
+{
+    var objectModels = [];
+
+    return objectModels;
+}
+
+RoadRash64.prototype.parseObjectPlacements = function()
+{
+    var objectPlacements = [];
+
+    for(var nFile = 0; nFile < NUM_PLACEMENT_FILES; nFile++)
+    {
+        var entryOffset = nFile * 4;
+        var offset = this.dvRom.getUint32(RA_OBJECT_PLACEMENT_TABLE + entryOffset);
+
+        if(offset == 0)
+        {
+            continue;
+        }
+
+        var raFile = RA_OBJECT_PLACEMENT_TABLE + entryOffset + offset;
+        var numPlacements = this.dvRom.getUint32(raFile);
+
+        for(var nPlacement = 0; nPlacement < numPlacements; nPlacement++)
+        {
+            var raPlacement = raFile + 0x88 + nPlacement * ObjectPlacement.SIZE;
+            var placement = new ObjectPlacement(this.dvRom, raPlacement);
+            objectPlacements.push(placement); // todo should probably subarray each file
+        }
+    }
+
+    return objectPlacements;
+}
+
+RoadRash64.prototype.parseItemTextures = function()
+{
+    var itemTextures = [];
+
+    for(var nTexture = 0; nTexture < NUM_ITEM_TEXTURES; nTexture++)
+    {
+        var raTexture = RA_ITEM_TEXTURES + nTexture * 0x1000;
+        var data = rgba32_texture_from_i8(this.dvRom, raTexture, 64, 64);
+        itemTextures.push(data);
+    }
+
+    return itemTextures;
+}
+
+RoadRash64.prototype.parseItemPlacements = function()
+{
+    var itemPlacements = [];
+
+    var numItems = this.dvRom.getUint32(RA_ITEM_COUNT);
+    
+    for(var nItem = 0; nItem < numItems; nItem++)
+    {
+        var raItem = RA_ITEM_TABLE + nItem * Item.SIZE;
+        var item = new Item(this.dvRom, raItem);
+        itemPlacements.push(item);
+    }
+
+    return itemPlacements;
+}
+
+RoadRash64.prototype.parseMapMesh = function(dvMesh)
+{
+    var subMeshes = [];
+
+    var meshHeader = new MeshHeader(dvMesh, 0);
+    
+    for(var nSubMesh = 0; nSubMesh < meshHeader.numSubMeshes; nSubMesh++)
+    {
+        var subMesh = {
+            info: null,
+            header: null,
+            geometryPackets: []
+        };
+
+        var entryOffset = 0xF0 + (nSubMesh * 12);
+        var subMeshInfo = new SubMeshInfo(dvMesh, entryOffset);
+        var subMeshOffset = subMeshInfo.subMeshOffset + entryOffset;
+        var subMeshHeader = new SubMeshHeader(dvMesh, subMeshOffset);
+
+        subMesh.info = subMeshInfo;
+        subMesh.header = subMeshHeader;
+
+        var packetOffset = subMeshOffset + SubMeshHeader.SIZE;
+
+        for(var nPacket = 0; nPacket < subMeshHeader.numPackets; nPacket++)
+        {
+            var packet = this.parseGeometryPacket(dvMesh, packetOffset);
+            packetOffset += packet.header.packetSize;
+
+            subMesh.geometryPackets.push(packet);
+        }
+
+        subMeshes.push(subMesh);
+    }
+
+    return subMeshes;
+
+    // collision
+    // todo optimize
+
+    //var collisionGeometry = new THREE.BufferGeometry();
+    //var collisionPositions = [];
+    //var collisionColors = [];
+//
+    //const surfaceTypeColors = [
+    //    /*00*/ null,
+    //    /*01*/ [1, 0, 0],
+    //    /*02*/ [0, 1, 0],
+    //    /*03*/ [0, 0, 1],
+    //    /*04*/ [1, 1, 0],
+    //    /*05*/ [1, 0, 1],
+    //    /*06*/ [0, 1, 1],
+    //    /*07*/ [1, 1, 1],
+    //    /*08*/ [0.5, 0, 0],
+    //    /*09*/ [0, 0.5, 0],
+    //    /*0A*/ [0, 0, 0.5],
+    //    /*0B*/ [0.5, 0.5, 0],
+    //    /*0C*/ [0.5, 0, 0.5],
+    //    /*0D*/ [0, 0.5, 0.5],
+    //    /*0E*/ [0.5, 0.5, 0.5]
+    //];
+//
+    //var collisionHeader = new CollisionHeader(dvMesh, meshHeader.offsCollision);
+    //for(var nGroup = 0; nGroup < collisionHeader.numGroups; nGroup++)
+    //{
+    //    var offsCollisionGroup = dvMesh.getUint16(meshHeader.offsCollision + CollisionHeader.SIZE + nGroup * 2);
+    //    var groupHeader = new CollisionGroupHeader(dvMesh, meshHeader.offsCollision + offsCollisionGroup);
+    //    var offsTriangleList = meshHeader.offsCollision + offsCollisionGroup + CollisionGroupHeader.SIZE;
+//
+    //    for(var nList = 0; nList < groupHeader.numTriangleLists; nList++)
+    //    {
+    //        var triangleListHeader = new CollisionTriangleListHeader(dvMesh, offsTriangleList);
+    //        offsTriangleList += CollisionTriangleListHeader.SIZE;
+//
+    //        var color = surfaceTypeColors[triangleListHeader.attributeId];
+//
+    //        for(var nTri = 0; nTri < triangleListHeader.numTriangles; nTri++)
+    //        {
+    //            var tri = new Triangle(dvMesh, offsTriangleList);
+    //            offsTriangleList += Triangle.SIZE;
+//
+    //            var vertex0 = new SPVertex(dvMesh, triangleListHeader.offsVertices + tri.v0 * SPVertex.SIZE);
+    //            var vertex1 = new SPVertex(dvMesh, triangleListHeader.offsVertices + tri.v1 * SPVertex.SIZE);
+    //            var vertex2 = new SPVertex(dvMesh, triangleListHeader.offsVertices + tri.v2 * SPVertex.SIZE);
+//
+    //            
+//
+    //            collisionPositions.push(-(meshHeader.worldX + vertex0.x), (meshHeader.worldZ + vertex0.z) / 2, (meshHeader.worldY + vertex0.y));
+    //            collisionPositions.push(-(meshHeader.worldX + vertex1.x), (meshHeader.worldZ + vertex1.z) / 2, (meshHeader.worldY + vertex1.y));
+    //            collisionPositions.push(-(meshHeader.worldX + vertex2.x), (meshHeader.worldZ + vertex2.z) / 2, (meshHeader.worldY + vertex2.y));
+//
+    //            collisionColors.push(color[0], color[1], color[2]);
+    //            collisionColors.push(color[0], color[1], color[2]);
+    //            collisionColors.push(color[0], color[1], color[2]);
+    //        }
+//
+    //        if((offsTriangleList % 4) != 0) offsTriangleList += 2;
+    //    }
+    //}
+//
+    //var fCollisionPositions = new Float32Array(collisionPositions);
+    //var fCollisionColors = new Float32Array(collisionColors);
+    //collisionGeometry.setAttribute('position', new THREE.BufferAttribute(fCollisionPositions, 3));
+    //collisionGeometry.setAttribute('color', new THREE.BufferAttribute(fCollisionColors, 3));
+    //var material = new THREE.MeshBasicMaterial({ wireframe: true, vertexColors: THREE.VertexColors });
+    //var collisionMesh = new THREE.Mesh(collisionGeometry, material);
+    //this.sceneAdd('collision', [ collisionMesh ]);
+}
+
+RoadRash64.prototype.parseTextureFile = function(dvTexture)
+{
+    // TODO handle animated textures
+    var textureHeader = new TextureHeader(dvTexture, 0);
+    var ciOffset = TextureHeader.SIZE;
+    var width = textureHeader.width;
+    var height = textureHeader.height;
+
+    var data = null;
+
+    if(textureHeader.format == TEX_FMT_CI4)
+    {
+        var tlutOffset = ciOffset + (width * height) / 2;
+        data = rgba32_texture_from_ci4(dvTexture, ciOffset, tlutOffset, width, height);
+    }
+    else if(textureHeader.format == TEX_FMT_CI8)
+    {
+        var tlutOffset = ciOffset + (width * height);
+        data = rgba32_texture_from_ci8(dvTexture, ciOffset, tlutOffset, width, height);
+    }
+    else
+    {
+        return null;
+    }
+
+    return { data: data, width: width, height: height, flags: textureHeader.unk24 };
+}
+
+RoadRash64.prototype.getModelIndex = function(modelId_hi, modelId_lo)
+{
+    var indexBase = 0;
+    for(var i = 0; i < modelId_hi; i++)
+    {
+        indexBase += this.dvRom.getUint32(RA_MODEL_CATEGORY_INDICES + i * 4);
+    }
+
+    return indexBase + modelId_lo;
+}
+
+RoadRash64.prototype.parseGeometryPacket = function(dv, packetOffset)
+{
+    var packet = {
+        header: null,
+        vertices: [],
+        triangles: []
+    };
+
+    packet.header = new SubMeshPacketHeader(dv, packetOffset);
+    //packet.header = packetHeader
+
+    var offsVertices = packetOffset + SubMeshPacketHeader.SIZE;
+    var offsTriangles = offsVertices + packet.header.verticesSize;
+
+    for(var nVertex = 0; nVertex < packet.header.numVertices; nVertex++)
+    {
+        var vertex = new SPVertex(dv, offsVertices + (nVertex * SPVertex.SIZE));
+        packet.vertices.push(vertex);
+    }
+
+    for(var nTri = 0; nTri < packet.header.numTriangles; nTri++)
+    {
+        var tri = new Triangle(dv, offsTriangles + (nTri * Triangle.SIZE));
+        packet.triangles.push(tri);
+    }
+
+    return packet;
+}
+
+//////////////
 
 function SPVertex(dv, offset)
 {
